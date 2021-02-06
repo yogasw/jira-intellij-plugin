b18b932791975417bee20a29ed73b91bd0a530ca
aromano <asaa@paradigmadigital.com>
2021-02-06 11:59:51 +0100

    removed deprecated notifications methods

--- src/main/java/com/intellij/jira/components/JiraNotificationManager.java
+++ src/main/java/com/intellij/jira/components/JiraNotificationManager.java
@@ -2,32 +2,35 @@ package com.intellij.jira.components;
 
 import com.intellij.notification.Notification;
 import com.intellij.notification.NotificationGroup;
+import com.intellij.notification.NotificationGroupManager;
 import com.intellij.openapi.components.ServiceManager;
 
-import static com.intellij.notification.NotificationDisplayType.STICKY_BALLOON;
 import static com.intellij.notification.NotificationType.ERROR;
 import static com.intellij.notification.NotificationType.INFORMATION;
 
 public class JiraNotificationManager {
 
-    private static final NotificationGroup BALLON_NOTIFICATION_GROUP = NotificationGroup.balloonGroup("Jira Notifications");
-    private static final NotificationGroup STICKY_BALLOON_NOTIFICATION_GROUP = new NotificationGroup("Jira Notifications", STICKY_BALLOON, true);
-
+    private static final String BALLON_NOTIFICATION_GROUP_NAME = "Jira Balloon Notifications";
+    private static final String STICKY_BALLON_NOTIFICATION_GROUP_NAME = "Jira Sticky Balloon Notifications";
 
     public static JiraNotificationManager getInstance(){
         return ServiceManager.getService(JiraNotificationManager.class);
     }
 
     public Notification createNotification(String title, String content){
-        return BALLON_NOTIFICATION_GROUP.createNotification(title, null, content, INFORMATION);
+        return getNotificationGroup(BALLON_NOTIFICATION_GROUP_NAME).createNotification(title, null, content, INFORMATION);
     }
 
     public Notification createNotificationError(String title, String content){
-        return STICKY_BALLOON_NOTIFICATION_GROUP.createNotification(title, null, content, ERROR);
+        return getNotificationGroup(STICKY_BALLON_NOTIFICATION_GROUP_NAME).createNotification(title, null, content, ERROR);
     }
 
     public Notification createSilentNotification(String title, String content){
-        return BALLON_NOTIFICATION_GROUP.createNotification(title, null, content, INFORMATION);
+        return getNotificationGroup(BALLON_NOTIFICATION_GROUP_NAME).createNotification(title, null, content, INFORMATION);
+    }
+
+    private static NotificationGroup getNotificationGroup(String name) {
+        return NotificationGroupManager.getInstance().getNotificationGroup(name);
     }
 
 }
