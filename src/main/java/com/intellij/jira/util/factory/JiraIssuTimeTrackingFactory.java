bfa2b5228245567f42749e585c8f0b841102cfb2
aromano <asaa@paradigmadigital.com>
2021-09-23 08:55:40 +0200

    remove unused classes

--- src/main/java/com/intellij/jira/util/factory/JiraIssuTimeTrackingFactory.java
+++
@@ -1,9 +0,0 @@
-package com.intellij.jira.util.factory;
-
-import com.intellij.jira.rest.model.JiraIssueTimeTracking;
-import com.intellij.openapi.util.Factory;
-
-@FunctionalInterface
-public interface JiraIssuTimeTrackingFactory extends Factory<JiraIssueTimeTracking> {
-
-}
--- src/main/java/com/intellij/jira/util/factory/JiraIssueCommentFactory.java
+++
@@ -1,9 +0,0 @@
-package com.intellij.jira.util.factory;
-
-import com.intellij.jira.rest.model.JiraIssueComment;
-import com.intellij.openapi.util.Factory;
-
-@FunctionalInterface
-public interface JiraIssueCommentFactory extends Factory<JiraIssueComment> {
-
-}
